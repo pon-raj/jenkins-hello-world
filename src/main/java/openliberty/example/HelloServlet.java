a3948b2ae9742aa647aeeab17f355aa251a6a62a
pon-raj <pon_raj0120@yahoo.com>
2021-01-09 19:00:24 -0500

    Update HelloServlet.java

--- src/main/java/openliberty/example/HelloServlet.java
+++ src/main/java/openliberty/example/HelloServlet.java
@@ -1,1 +1,26 @@
-test
+package openliberty.example;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/servlet")
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
+        // tag::responsestring[]
+        response.getWriter().append("Hello! Is Gradle working for you?\n");
+        // end::responsestring[]
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
